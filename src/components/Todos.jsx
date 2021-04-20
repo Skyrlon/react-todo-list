b7ffc8f1ca49ee6934ab13c2cdd40aaa21f12466
Antoine Drouault <antoinedrouault@hotmail.com>
2021-04-20 18:11:59 +0200

    added styled-component package, added style in Todos component

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,13 +1,30 @@
+import styled from "styled-components";
+
+const StyledTodos = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  margin-top: 5em;
+
+  & > div {
+    border: 1px solid black;
+    width: 15%;
+    margin-left: 2.5%;
+    margin-right: 2.5%;
+  }
+`;
+
 const Todos = ({ TodoListItems }) => {
   return (
-    <div className="todos">
+    <StyledTodos>
       {TodoListItems.map((element) => (
         <div key={element.id}>
           <h3>{element.title}</h3>
           <div>{element.text}</div>
         </div>
       ))}
-    </div>
+    </StyledTodos>
   );
 };
 
