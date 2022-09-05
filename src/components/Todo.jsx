9a6709ca586694db3b4d07d37a077b6cc11a16a7
Antoine Drouault <antoinedrouault@hotmail.com>
2022-09-05 23:58:26 +0200

    Make new design for todo with mui

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,17 +1,15 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEdit, faTrash, faCheck } from "@fortawesome/free-solid-svg-icons";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import { Card, CardContent, Typography, CardActions } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import CheckIcon from "@mui/icons-material/Check";
 
-const StyledTodo = styled.div`
+const StyledTodo = styled(Card)`
   border: 1px solid black;
   width: 100%;
   height: 100%;
   overflow-wrap: break-word;
-  background-color: ${(props) =>
-    (props.priority === "hight" && "red") ||
-    (props.priority === "medium" && "yellow") ||
-    (props.priority === "low" && "green")};
   &.done {
     background-color: grey;
   }
@@ -27,7 +25,12 @@ const Todo = ({
 }) => {
   return (
     <StyledTodo
-      priority={todo.priority}
+      sx={{
+        backgroundColor:
+          (todo.priority === "hight" && "red") ||
+          (todo.priority === "medium" && "yellow") ||
+          (todo.priority === "low" && "green"),
+      }}
       className={todo.completed ? " done" : ""}
     >
       {showCheckBoxes && (
@@ -36,29 +39,37 @@ const Todo = ({
           onChange={(e) => selectedToBeDeleted(e.target.checked, todo.id)}
         />
       )}
-      <h3>{todo.title}</h3>
-      <div>{todo.details}</div>
-      <FontAwesomeIcon
-        icon={faEdit}
-        onClick={(e) => {
-          e.stopPropagation();
-          onEdit();
-        }}
-      />
-      <FontAwesomeIcon
-        icon={faTrash}
-        onClick={(e) => {
-          e.stopPropagation();
-          onDelete();
-        }}
-      />
-      <FontAwesomeIcon
-        icon={faCheck}
-        onClick={(e) => {
-          e.stopPropagation();
-          onComplete();
-        }}
-      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {todo.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {todo.details}
+        </Typography>
+      </CardContent>
+
+      <CardActions
+        sx={{ display: "flex", flexDirection: "row", justifyContent: "center" }}
+      >
+        <EditIcon
+          onClick={(e) => {
+            e.stopPropagation();
+            onEdit();
+          }}
+        />
+        <DeleteIcon
+          onClick={(e) => {
+            e.stopPropagation();
+            onDelete();
+          }}
+        />
+        <CheckIcon
+          onClick={(e) => {
+            e.stopPropagation();
+            onComplete();
+          }}
+        />
+      </CardActions>
     </StyledTodo>
   );
 };
